19fe193636f0271935d2529e08a3505b6b734b60
NobreDev11 <nobre.dev11@gmail.com>
2025-05-09 18:34:48 -0300

    Adiciona rota para buscar cliente por ID

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -5,8 +5,19 @@ exports.criarCliente = async (req, res) => {
     const { nome, email, telefone } = req.body;
     const novoCliente = new Cliente({ nome, email, telefone });
     await novoCliente.save();
-    res.status(201).json({ message: 'Cliente cadastrado com sucesso!' });
+    res.status(201).json({ message: 'Cliente cadastrado com sucesso!', cliente: novoCliente });
   } catch (error) {
     res.status(500).json({ message: 'Erro ao cadastrar cliente', error });
+  }
+};
+
+exports.buscarClientePorId = async (req, res) => {
+  try {
+    const cliente = await Cliente.findById(req.params.id);
+    if (!cliente) return res.status(404).json({ message: 'Cliente não encontrado' });
+
+    res.json(cliente);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar cliente', error });
   }
 };
--- routes/clienteRoutes.js
+++ routes/clienteRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const clienteController = require('../controllers/clienteController');
 
 router.post('/clientes', clienteController.criarCliente);
+router.get('/clientes/:id', clienteController.buscarClientePorId);
 
 module.exports = router;
