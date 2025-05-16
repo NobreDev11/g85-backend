3a86e475d816cb5995056658eaaae0d4aa9cec1d
NobreDev11 <nobre.dev11@gmail.com>
2025-05-16 07:10:39 -0300

    Adiciona função registrarUsuario ao usuariosController

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,4 +1,15 @@
-exports.listarUsuarios = (req, res) => {
-    res.json([{ nome: 'Usuário teste', email: 'teste@email.com' }]);
-  };
-  
+// controllers/usuariosController.js
+
+exports.registrarUsuario = (req, res) => {
+  const { nome, email, password } = req.body;
+
+  // Apenas simulação (você pode integrar com MongoDB depois)
+  if (!nome || !email || !password) {
+    return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+  }
+
+  res.status(201).json({
+    message: 'Usuário registrado com sucesso!',
+    usuario: { nome, email }
+  });
+};
