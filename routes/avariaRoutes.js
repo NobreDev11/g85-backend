6ef898b7b2101ac585018be9b105e7bf28505754
NobreDev11 <nobre.dev11@gmail.com>
2025-05-19 23:35:42 -0300

    Força atualização do avariaRoutes.js após correção de tradução

--- routes/avariaRoutes.js
+++ routes/avariaRoutes.js
@@ -7,3 +7,6 @@ router.post('/avarias', authMiddleware, avariaController.criarAvaria);
 router.get('/avarias', authMiddleware, avariaController.listarAvarias);
 
 module.exports = router;
+
+// no final do arquivo avariaRoutes.js
+// Fix commit trigger
